0a363e8cd7cfde570575bf2d2fa45925d47f8a76
Jorge Kleinerman <jkleinerman@gmail.com>
2015-04-15 17:10:20 -0300

    -Comments to main.py -Fixing bug creating event field pssgId as Str. Setting it as Int type

--- controller/py_src/main.py
+++ controller/py_src/main.py
@@ -42,15 +42,15 @@ class Controller(object):
         self.logger.addHandler(loggingHandler)
 
         self.dataBase = database.DataBase(DB_FILE)
-        self.ioIfaceQue=posix_ipc.MessageQueue(QUEUE_FILE, posix_ipc.O_CREAT)
+        
+        #Defining a OS queue to get messages from the ioiface
+        self.ioIfaceQue = posix_ipc.MessageQueue(QUEUE_FILE, posix_ipc.O_CREAT)
 
+        #Dictionary containing functions to be launched according to the messages received by the ioiface
         self.handlers = { 'card'   : self.procCard,
                           'buttom' : self.procButtom,
                           'state'  : self.procState
                         }
-
-        #Exit flag to notify threads to finish
-        self.exitFlag = threading.Event()
 
         #Queue used to send events to eventThread
         self.mainToEvent = queue.Queue()
@@ -58,17 +58,17 @@ class Controller(object):
         #Queue used to send responses from netMngr thread to event thread
         netToEvent = queue.Queue()
 
-
         #Queue used to send responses from netMngr thread to ReSender thread
         netToReSnd = queue.Queue()
 
+        #Exit flag to notify threads to finish
+        self.exitFlag = threading.Event()
 
         #Creating the Net Manager Thread 
         self.netMngr = network.NetMngr(netToEvent, netToReSnd, self.exitFlag)        
 
         #Creating the Event Manager Thread giving to it the previous event queue
         self.eventMngr = events.EventMngr(self.mainToEvent, self.netMngr, netToEvent, netToReSnd, self.exitFlag)
-
 
         #Registering "sigtermHandler" handler to act when receiving the SIGTERM signal
         signal.signal(signal.SIGTERM, self.sigtermHandler)
@@ -83,17 +83,29 @@ class Controller(object):
 
 
 
+    #---------------------------------------------------------------------------#
+
+    def sigtermHandler(self, signal, frame):
+
+        try:
+            self.logger.info('Getting SIGTERM.')
+            self.ioIfaceQue.unlink()
+            self.ioIfaceQue.close()
+
+        except posix_ipc.ExistentialError:
+            self.logger.info('An earlier SIGTERM signal is being processed.')
+
+
+
+    #---------------------------------------------------------------------------#
 
     def getIoIfaceArgs(self):
         '''
-        This method return a string with all arguments for the pssg-iface
-        binary.
+        This method return a string with all arguments to launch ioiface binary.
         They are got from Passage table of local DataBase
         '''
 
-
         ioIfaceArgs = ''
-
 
         for pssgId in self.pssgsParams:
 
@@ -106,10 +118,12 @@ class Controller(object):
 
 
 
+    #---------------------------------------------------------------------------#
+
     def launchIoIface(self):
         '''
         Launch Pssg Iface binary.
-        Return a process object
+        Returns a process object
         '''
 
         ioIfaceCmd = '{} {}'.format(IOIFACE_BIN, self.getIoIfaceArgs())
@@ -118,28 +132,14 @@ class Controller(object):
         self.logger.debug(logMsg)
 
         ioIfaceProc = subprocess.Popen(ioIfaceCmd, shell=True, 
-                                         stdout=subprocess.PIPE, 
-                                         stderr=subprocess.STDOUT
-                                        )
-
+                                       stdout=subprocess.PIPE, 
+                                       stderr=subprocess.STDOUT
+                                       )
         return ioIfaceProc
-        
 
 
 
-    def sigtermHandler(self, signal, frame):
-
-        try:
-            self.logger.info('Getting SIGTERM.')
-            self.ioIfaceQue.unlink()
-            self.ioIfaceQue.close()
-        
-        except posix_ipc.ExistentialError:
-            self.logger.info('An earlier SIGTERM signal is being processed.')
-
-        #self.exitFlag.set()
-
-
+    #---------------------------------------------------------------------------#
 
     def procCard(self, pssgId, side, cardNumber):
         '''
@@ -149,8 +149,8 @@ class Controller(object):
         allowed, personId, notReason = self.dataBase.canAccess(pssgId, side, cardNumber)
 
         if allowed:
-            #Open the pssg as soon as posible
-            print('Opening the pssg...')
+            #Open the passage as soon as posible
+            print('Opening the passage...')
 
 
         dateTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
@@ -169,14 +169,22 @@ class Controller(object):
         self.mainToEvent.put(event)
 
 
+
+    #---------------------------------------------------------------------------#
+
     def procButtom(self, pssgId, state):
         print('procButtom', pssgId, state)
 
+
+
+    #---------------------------------------------------------------------------#
 
     def procState(self, pssgId, state):
         print('procState', pssgId, state)
 
 
+
+    #---------------------------------------------------------------------------#
 
     def run(self):
 
@@ -197,6 +205,7 @@ class Controller(object):
                 ioIfaceData = self.ioIfaceQue.receive()
                 ioIfaceData = ioIfaceData[0].decode('utf8')
                 pssgId, side, varField = ioIfaceData.split(';')
+                pssgId = int(pssgId)
                 side = int(side)
                 command, value  = varField.split('=')
                 self.handlers[command](pssgId, side, value)
@@ -221,7 +230,10 @@ class Controller(object):
         sys.exit(self.exitCode)
 
 
+    #---------------------------------------------------------------------------#    
 
+
+#---- MAIN EXECUTION ---#
     
 controller = Controller()
 controller.run()
