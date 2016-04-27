e4f785f4ca3ccf2f10149d85c6edf17494ec49be
Ary Kleinerman <kleinerman@gmail.com>
2016-04-27 11:50:20 -0300

    Add REST api authentication

--- server/back_end/crud.py
+++ server/back_end/crud.py
@@ -11,11 +11,14 @@ import queue
 import genmngr
 from config import *
 
-
 import sys
 
 from flask import Flask, jsonify, request, abort
 import time
+
+# constants
+BAD_REQUEST = 400
+CREATED = 201
 
 
 class CrudMngr(genmngr.GenericMngr):
@@ -30,8 +33,6 @@ class CrudMngr(genmngr.GenericMngr):
         self.dbMngr = dbMngr
 
 
-
-
     #---------------------------------------------------------------------------#
 
     def run(self):
@@ -40,20 +41,24 @@ class CrudMngr(genmngr.GenericMngr):
         It launches the Flask server and it waits for REST request from the GUI
         '''
 
-
         app = Flask(__name__)
 
+        @auth.get_password
+        def get_password(username):
+            if username == 'conpass':
+                return 'p4ssw0rd'
+            else:
+                return None
+
         @app.route('/api/v1.0/organization', methods=['POST'])
+        @auth.login_required
         def addOrganization():
             if not request.json or not 'name' in request.json:
-                abort(400)
-
+                abort(BAD_REQUEST)
+                
             self.dbMngr.addOrganization(request.json)
+            return jsonify({'xxx': 'xxx'}), CREATED
 
-            return jsonify({'1': 1}), 201
-
-
-
-
+        # run the server
         app.run(debug=True, use_reloader=False, host="0.0.0.0", port=5000, threaded=True)
 
