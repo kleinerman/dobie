79a2125dc69aa8ecfbb556957139bf8c6a869e0a
Ary Kleinerman <kleinerman@gmail.com>
2016-05-11 13:03:56 -0300

    implement error handling and global authentication

--- server/back_end/crud.py
+++ server/back_end/crud.py
@@ -23,7 +23,24 @@ BAD_REQUEST = 400
 CREATED = 201
 OK = 200
 USER = 'conpass'
-PASSWD = 'p4sw0rd'
+PASSWD = 'password'
+
+
+
+class BadRequest(Exception):
+    def __init__(self, message, status_code=400):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+
+
+
+class InternalError(Exception):
+    def __init__(self, message, status_code=500):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+
 
 
 class CrudMngr(genmngr.GenericMngr):
@@ -49,6 +66,28 @@ class CrudMngr(genmngr.GenericMngr):
         app = Flask(__name__)
         auth = HTTPBasicAuth()
 
+        
+        # Error hanlders definition
+        @app.errorhandler(BadRequest)
+        def badRequest(error):
+            response = jsonify ({'error': 'bad request', 'message': error.message, 'code':error.status_code})
+            response.status_code = error.status_code
+            return response
+
+        @app.errorhandler(404)
+        def notFound(error):
+            response = jsonify ({'error': 'request not found', 'message': 'Not found', 'code':404})
+            response.status_code = 404
+            return response
+
+        @app.errorhandler(InternalError)
+        def internalServerError(error):
+            response = jsonify ({'error': 'internal server error', 'message': 'Not found', 'code':404})
+            response.status_code = 500
+            return response
+
+
+        # For API authentication
         @auth.get_password
         def get_password(username):
             if username == USER:
@@ -56,38 +95,45 @@ class CrudMngr(genmngr.GenericMngr):
             else:
                 return None
 
-        @app.route('/api/v1.0/organization', methods=['POST', 'PUT','DELETE'])
+
+        # Global API protection:
+        # before_request decorator registers a function that runs before requests.
+        # Decorating with login_required this function it avoids to decorate every route in the API
+        @app.before_request
         @auth.login_required
+        def before_request():
+                pass
+
+
+        @app.route('/api/v1.0/organization', methods=['POST', 'PUT','DELETE'])
         def crudOrganization():
             try:
                 if request.method == 'POST':
-                    necessaryKeys = ('name',)
-                    if not all(key in request.json for key in necessaryKeys):
-                        abort(BAD_REQUEST)
+                    # if this json key does not exist, it will raise a BadRequest exception
+                    necessaryKeys = request.json['name']
                     self.dbMngr.addOrganization(request.json)
                     return jsonify({'1': 1}), CREATED
 
                 elif request.method == 'PUT':
-                    necessaryKeys = ('id', 'name')
-                    if not all(key in request.json for key in necessaryKeys):
-                        abort(BAD_REQUEST)
+                    necessaryKeys = request.json['id'], request.json['name']
                     self.dbMngr.updOrganization(request.json)
                     return jsonify({'1': 1}), OK
 
                 elif request.method == 'DELETE':
-                    necessaryKeys = ('id',)
-                    if not all(key in request.json for key in necessaryKeys):
-                        abort(BAD_REQUEST)
+                    necessaryKeys = request.json['id']
                     self.dbMngr.delOrganization(request.json)
                     return jsonify({'1': 1}), OK
 
 
-            except database.OrganizationError as organizationError:
-                return jsonify({'1': 1}), 500
-
-
-
-
+            except database.OrganizationError as e:
+                raise InternalError(e)
+            except KeyError as e:
+                raise BadRequest("Invalid Organization: missing '{}'".format(e.args[0]))
+            except TypeError:
+                raise BadRequest(('Expecting to find application/json in Content-Type header '
+                                  '- the server could not comply with the request since it is '          
+                                  'either malformed or otherwise incorrect. The client is assumed '
+                                  'to be in error'))
 
 
         @app.route('/api/v1.0/zone', methods=['POST', 'PUT','DELETE'])
