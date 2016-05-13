1b904a36ddc1cb70e7c48a3ba18fdb30ba9c24b3
Jorge Kleinerman <jkleinerman@gmail.com>
2016-05-13 17:38:58 -0300

    Create the msg to add passage to send the controller

--- server/back_end/netpassage.py
+++ server/back_end/netpassage.py
@@ -1,44 +1,11 @@
-import threading
 import logging
-import datetime
-import time
-import os
 
-import select
 import socket
 import json
 
-import database
-import queue
 
-import genmngr
+from network import *
 from config import *
-
-
-import sys
-
-
-
-int_CON  = 0x01
-int_RCON = 0x02
-int_EVT  = 0x03
-int_REVT = 0x04
-int_EVS  = 0x05
-int_REVS = 0x06
-int_CUD  = 0x07
-int_RCUD = 0x08
-int_END  = 0x1F
-
-
-CON  = bytes([int_CON])
-RCON = bytes([int_RCON])
-EVT  = bytes([int_EVT])
-REVT = bytes([int_REVT])
-EVS  = bytes([int_EVS])
-REVS = bytes([int_REVS])
-CUD  = bytes([int_CUD])
-RCUD = bytes([int_RCUD])
-END  = bytes([int_END])
 
 
 
@@ -58,4 +25,8 @@ class NetPassage(object):
     def addPassage(self, ctrllerMac, passage):
         '''
         '''
-        print(ctrllerMac, passage)
+        passageJson = json.dumps(passage).encode('utf8')
+
+        msg = CUD + b'S' + b'C' + REVS + passageJson + END 
+        
+        self.netMngr.sendToCtrller(msg, ctrllerMac)
