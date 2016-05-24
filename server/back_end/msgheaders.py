81fe4d4d114463ea53be381257b490824899a94e
Jorge Kleinerman <jkleinerman@gmail.com>
2016-05-24 17:19:23 -0300

    -Put message headers in a different file (msgheaders.py) to be able to import them from network.py and msgreceiver.py files avoiding import file network.py from msgreceiver.py -Delegate the responsibility to convert event messages in event list to MsgReceiver class. This same class will receive CRUD messages and introduce them in the local database

---
+++ server/back_end/msgheaders.py
@@ -0,0 +1,20 @@
+int_CON  = 0x01
+int_RCON = 0x02
+int_EVT  = 0x03
+int_REVT = 0x04
+int_EVS  = 0x05
+int_REVS = 0x06
+int_CUD  = 0x07
+int_RCUD = 0x08
+int_END  = 0x1F
+
+
+CON  = bytes([int_CON])
+RCON = bytes([int_RCON])
+EVT  = bytes([int_EVT])
+REVT = bytes([int_REVT])
+EVS  = bytes([int_EVS])
+REVS = bytes([int_REVS])
+CUD  = bytes([int_CUD])
+RCUD = bytes([int_RCUD])
+END  = bytes([int_END])
--- server/back_end/network.py
+++ server/back_end/network.py
@@ -14,32 +14,7 @@ import genmngr
 import database
 import msgreceiver
 from config import *
-
-
-
-
-int_CON  = 0x01
-int_RCON = 0x02
-int_EVT  = 0x03
-int_REVT = 0x04
-int_EVS  = 0x05
-int_REVS = 0x06
-int_CUD  = 0x07
-int_RCUD = 0x08
-int_END  = 0x1F
-
-
-CON  = bytes([int_CON])
-RCON = bytes([int_RCON])
-EVT  = bytes([int_EVT])
-REVT = bytes([int_REVT])
-EVS  = bytes([int_EVS])
-REVS = bytes([int_REVS])
-CUD  = bytes([int_CUD])
-RCUD = bytes([int_RCUD])
-END  = bytes([int_END])
-
-
+from msgheaders import *
 
 
 
@@ -154,24 +129,18 @@ class NetMngr(genmngr.GenericMngr):
         #It should be delivered to "eventMngr" thread.
         if msg.startswith(EVT):
             response = REVT + b'OK' + END
-
-            event = msg.strip(EVT+END).decode('utf8')
-            event = json.loads(event)
-            events = [event]
-            self.msgReceiver.netToMsgRec.put(events)
             self.sendToCtrller(response, scktFd=fd)
-
+            self.msgReceiver.netToMsgRec.put(msg)
 
 
         elif msg.startswith(EVS):
             response = REVS + b'OK' + END
-
-            events = msg[1:-1].split(EVS)
-            events = [json.loads(evnt.decode('utf8')) for evnt in events]
-            self.msgReceiver.netToMsgRec.put(events)
             self.sendToCtrller(response, scktFd=fd)
-            
+            self.msgReceiver.netToMsgRec.put(msg)
 
+
+        elif msg.startswith(RCUD):
+            self.msgReceiver.netToMsgRec.put(msg)
 
 
     #---------------------------------------------------------------------------#
