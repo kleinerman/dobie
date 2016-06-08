1e84107c8ba8ab9ee579f94f5c57186851fadfc4
Jorge Kleinerman <jkleinerman@gmail.com>
2016-06-08 17:06:33 -0300

    Add rlseTime, bzzrTime and alrmTime to updPassage in database.py

--- server/back_end/database.py
+++ server/back_end/database.py
@@ -459,11 +459,13 @@ class DataBase(object):
         '''
 
         sql = ("UPDATE Passage SET i0In = {}, i1In = {}, o0In = {}, o1In = {}, "
-               "bttnIn = {}, stateIn = {}, rlseOut = {}, bzzrOut = {}, zoneId = {}, "
+               "bttnIn = {}, stateIn = {}, rlseOut = {}, bzzrOut = {}, "
+               "rlseTime = {}, bzzrTime = {}, alrmTime = {}, zoneId = {}, "
                "controllerId = {}, rowStateId = {} WHERE id = {}"
                "".format(passage['i0In'], passage['i1In'], passage['o0In'],
                          passage['o1In'], passage['bttnIn'], passage['stateIn'],
-                         passage['rlseOut'], passage['bzzrOut'], passage['zoneId'],
+                         passage['rlseOut'], passage['bzzrOut'], passage['rlseTime'], 
+                         passage['bzzrTime'], passage['alrmTime'], passage['zoneId'],
                          passage['controllerId'], TO_COMMIT, passage['id'])
               )
 
