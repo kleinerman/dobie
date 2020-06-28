53f8c81d3e16b73c8c3ec04ab2aa78ba49094379
Jorge Kleinerman <jkleinerman@gmail.com>
2020-06-28 11:20:37 -0300

    Fix in controller uninstall script
    Remove the usage of sudo to avoid having this package
    installed when running the controller without the
    server inside.
    When running server inside the controller, "sudo"
    package will be needed for server script installation.

--- controller/scripts/uninstall-dobie-c.sh
+++ controller/scripts/uninstall-dobie-c.sh
@@ -5,20 +5,20 @@ echo "Dobie Server Uninstallation Script"
 echo "=================================="
 
 echo "Stoping Dobie Controller systemd unit.."
-sudo systemctl stop dobie-c.service
+systemctl stop dobie-c.service
 
 echo "Disabling and removing systemd units.."
-sudo systemctl disable dobie-c.service
-sudo rm /etc/systemd/system/dobie-c.service
-sudo systemctl daemon-reload
+systemctl disable dobie-c.service
+rm /etc/systemd/system/dobie-c.service
+systemctl daemon-reload
 
 echo "Removing Dobie Controller DB directory"
-sudo rm -rf /var/lib/dobie-c/
+rm -rf /var/lib/dobie-c/
 
 echo "Removing Dobie Controller Logs directory.."
-sudo rm -rf /var/log/dobie-c/
+rm -rf /var/log/dobie-c/
 
 echo "Removing Log rotation file.."
-sudo rm /etc/logrotate.d/dobie-c
+rm /etc/logrotate.d/dobie-c
 
 
