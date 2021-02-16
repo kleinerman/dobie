8005367f62b1e927016672bb01f9365acd5e4513
Jorge Kleinerman <jkleinerman@gmail.com>
2021-02-15 21:14:58 -0300

    Finishing state sensor functionality

--- controller/c_src/state_snsr.c
+++ controller/c_src/state_snsr.c
@@ -19,7 +19,7 @@ int init_state_snsr(state_snsr_t * state_snsr_p,
 				unsigned int door_id,
 				struct timespec *event_wait_time_p){
     
-	sd_journal_print(LOG_NOTICE, "state_snsr of door: %d using GPIO: %d\n",
+	sd_journal_print(LOG_NOTICE, "State sensor of door: %d using GPIO: %d\n",
 	                 door_id, gpio_num);
 	state_snsr_p->chip = chip;
 	state_snsr_p->gpio_num = gpio_num;
@@ -37,14 +37,14 @@ int enable_state_snsr(state_snsr_t * state_snsr_p) {
 	int ret = 0;
 	state_snsr_p->b_line = gpiod_chip_get_line(state_snsr_p->chip, state_snsr_p->gpio_num);
 	if (!state_snsr_p->b_line) {
-		sd_journal_print(LOG_ALERT, "Error getting line of GPIO: %d for state_snsr of door: %d\n", 
+		sd_journal_print(LOG_ALERT, "Error getting line of GPIO: %d for state sensor of door: %d\n",
 		                 state_snsr_p->gpio_num, state_snsr_p->door_id);
 		ret = -1;
     }
 
-	ret = gpiod_line_request_falling_edge_events(state_snsr_p->b_line, CONSUMER);
+	ret = gpiod_line_request_both_edges_events(state_snsr_p->b_line, CONSUMER);
 	if (ret < 0) {
-		sd_journal_print(LOG_ALERT, "Failing to request event notification for state_snsr of door: %d\n",
+		sd_journal_print(LOG_ALERT, "Failing to request event notification for state sensor of door: %d\n",
 		                 state_snsr_p->door_id);
 		ret = -1;
     }
@@ -60,10 +60,12 @@ int enable_state_snsr(state_snsr_t * state_snsr_p) {
 
 void *run_state_snsr (void *arg_p){
 	int ret;
+	int current_state, new_state;
 	state_snsr_t *state_snsr_p = (state_snsr_t*) arg_p;
     char q_msg [50];
 	enable_state_snsr(state_snsr_p);
 	
+	current_state = ! gpiod_line_get_value(state_snsr_p->b_line);
 
 	while ( !exit_flag ) {
 		
@@ -75,12 +77,14 @@ void *run_state_snsr (void *arg_p){
 				gpiod_line_release(state_snsr_p->b_line);
 				usleep(BOUNCE_TIME);
 				enable_state_snsr(state_snsr_p);
-				ret = gpiod_line_get_value(state_snsr_p->b_line);
+				new_state = ! gpiod_line_get_value(state_snsr_p->b_line);
 
-				if (BUTTON_PRESSED == ret) {
-                    sd_journal_print(LOG_NOTICE, "State_snsr: %d pressed\n", state_snsr_p->door_id);
+				if (new_state != current_state) {
+					current_state = new_state;
+                    sd_journal_print(LOG_NOTICE, "State sensor: %d detected state: %d\n",
+					                 state_snsr_p->door_id, current_state);
 					pthread_mutex_lock(&mq_mutex);
-                        sprintf(q_msg, "%d;0;state=1", state_snsr_p->door_id);
+                        sprintf(q_msg, "%d;0;state=%d", state_snsr_p->door_id, current_state);
 						ret = mq_send(mq, q_msg, strlen(q_msg), 1);
 						if ( ret == 0 )
 							sd_journal_print(LOG_DEBUG, "SUCCESS Sending to queue: %s\n", q_msg);
@@ -89,20 +93,20 @@ void *run_state_snsr (void *arg_p){
 					pthread_mutex_unlock(&mq_mutex);
 				} 	
 				else
-					sd_journal_print(LOG_INFO, "Noise in state_snsr: %d\n", state_snsr_p->door_id);
+					sd_journal_print(LOG_INFO, "Noise in state sensor: %d\n", state_snsr_p->door_id);
 				break;
 
 			case 0:
-				sd_journal_print(LOG_DEBUG, "Thread of state_snsr: %d checking for exit\n", state_snsr_p->door_id);
+				sd_journal_print(LOG_DEBUG, "Thread of state sensor: %d checking for exit\n", state_snsr_p->door_id);
 				break;
 
 			default:
-				sd_journal_print(LOG_WARNING, "Unknown error on thread of state_snsr: %d\n", state_snsr_p->door_id);
+				sd_journal_print(LOG_WARNING, "Unknown error on thread of state sensor: %d\n", state_snsr_p->door_id);
 		}
 
 	}
-	sd_journal_print(LOG_NOTICE, "Thread of state_snsr: %d releasing GPIO: %d\n", state_snsr_p->door_id, state_snsr_p->gpio_num);
+	sd_journal_print(LOG_NOTICE, "Thread of state sensor: %d releasing GPIO: %d\n", state_snsr_p->door_id, state_snsr_p->gpio_num);
     gpiod_line_release(state_snsr_p->b_line);
-	sd_journal_print(LOG_NOTICE, "Thread of state_snsr: %d finished.", state_snsr_p->door_id);
+	sd_journal_print(LOG_NOTICE, "Thread of state sensor: %d finished.", state_snsr_p->door_id);
 
 }
