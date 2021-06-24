310a8d7420b2a5b18abc91eb727ee5c22fd9aa96
Jorge Kleinerman <jkleinerman@gmail.com>
2021-06-24 17:15:53 -0300

    Enhancement/Fix for Door CRUDs in controller
    -If the the server sends an incorrect doorNum when adding,
     updating or deleting a door, the controller will not crash.
     In this situation, an OK will be sent to the server anyway.
     This can happen if the server try to delete a door that
     never was created. For example: door added, controller not
     reachable (pending to add in server), force commit, then the
     user try to delete the door in the server when the controller
     is reachable again.
     Another situation could be a server sending an incorrect doorNum
     in any CRUD.

--- controller/py_src/crud.py
+++ controller/py_src/crud.py
@@ -10,6 +10,7 @@ import database
 import queue
 
 import genmngr
+import doormod
 from config import *
 from msgheaders import *
 
@@ -134,7 +135,13 @@ class CrudMngr(genmngr.GenericMngr):
         '''
         doorNum = int(doorJson['doorNum'])
         with self.lockDoors:
-            door = self.doors[doorNum]
+            try:
+                door = self.doors[doorNum]
+            except KeyError:
+                # This can happen if the server send an incorrect doorNum.
+                # In this situation, an OK will be sent to the server anyway.
+                self.logger.waring(f"There isn't any door with doorNum: {doorNum}")
+                return
             door.doorId = int(doorJson['id'])
             door.snsrType = int(doorJson['snsrType'])
             door.unlkTime = int(doorJson['unlkTime'])
@@ -151,7 +158,13 @@ class CrudMngr(genmngr.GenericMngr):
         '''
         doorNum = int(doorJson['doorNum'])
         with self.lockDoors:
-            door = self.doors[doorNum]
+            try:
+                door = self.doors[doorNum]
+            except KeyError:
+                # This can happen if the server send an incorrect doorNum.
+                # In this situation, an OK will be sent to the server anyway.
+                self.logger.waring(f"There isn't any door with doorNum: {doorNum}")
+                return
             door.doorId = int(doorJson['id'])
             door.snsrType = int(doorJson['snsrType'])
             door.unlkTime = int(doorJson['unlkTime'])
@@ -168,7 +181,17 @@ class CrudMngr(genmngr.GenericMngr):
         '''
         doorId = int(doorJson['id'])
         with self.lockDoors:
-            doorNum = self.doors.getDoorNum(doorId)
+            try:
+                doorNum = self.doors.getDoorNum(doorId)
+            except doormod.DoorNotConfigured as doorNotConfigured:
+                # This can happen if the server try to delete a door that
+                # never was created. For example: door added, controller not
+                # reachable (pending to add in server), force commit, then the
+                # user try to delete the door in the server.
+                # In this situation, an OK will be sent to the server anyway.
+                self.logger.debug(doorNotConfigured)
+                self.logger.warning("Trying to delete an unconfigured door")
+                return
             door = self.doors[doorNum]
             door.doorId = None
             door.snsrType = None
